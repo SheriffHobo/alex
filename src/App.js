84a6c6ec5e70bf04d5294a004e950c4b111e17f5
jackwade <6219292+jackwade@users.noreply.github.com>
2019-06-13 20:38:28 -0700

    react router and home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './Home';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      message: 'Oh Hi There',
-    };
-  }
-
-  render() {
-    return (
+function App() {
+  return (
+    <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <h1>{this.state.message}</h1>
-        </header>
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route component={Home} />
+        </Switch>
+
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
 }
 
 export default App;
