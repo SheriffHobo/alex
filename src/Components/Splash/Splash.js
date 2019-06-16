aef2710823d46b8d39afb95c857237460d9fc545
SheriffHobo <zombiemaster@gmail.com>
2019-06-16 15:32:45 -0700

    added react-materializae npm & css to splash

--- src/Components/Splash/Splash.js
+++ src/Components/Splash/Splash.js
@@ -3,8 +3,8 @@ import "./Style.css";
 
 const Splash = React.memo(props => {
 	return (
-		<div>
-			<img></img>
+		<div id="splash">
+			<img class="responsive-img" src="AlexBook.png" alt=""></img>
 			<h2>What's in your Library?</h2> 
 		</div>
 	);
