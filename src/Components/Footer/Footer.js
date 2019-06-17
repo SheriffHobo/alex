ac6145abb5ed56da67e7dfa371691a07ee68aab2
SheriffHobo <zombiemaster@gmail.com>
2019-06-16 20:33:48 -0700

    popmenu working

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import {Component} from 'react';
 import "./Style.css";
 
-const Footer = React.memo(props => {
-	return (
-		<footer className="Footer-sticky">
-      footer - this will get pushed down by the content, not usually visible
-      <br />privacy policy
-      <br />terms of use
-      <br />cookie policy
-      <br />careers at Alexandria 
+class Footer extends Component {
+
+  render() {
+  return (
+    <footer className="Footer-fixed">
+      <div>
+        <a><i id="menubtn" className="menu-btn noselect small material-icons dropdown-trigger" href='#' data-target='dropdown1'>menu</i></a>
+          <ul id='dropdown1' class='dropdown-content'>
+            <li><a href="#!">one</a></li>
+            <li><a href="#!">two</a></li>
+            <li class="divider" tabindex="-1"></li>
+            <li><a href="#!">three</a></li>
+            <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+            <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+          </ul>
+        <a><i id="homebtn" className="home-btn noselect small material-icons" onClick={() => alert('go to home')}>home</i></a>
+        <a><i id="chatbtn" className="chat-btn noselect small material-icons" onClick={() => alert('open chat')}>chat</i></a>
+      </div>
     </footer>
-	);
-});
+    );
+  };
+};
 
 export default Footer;
