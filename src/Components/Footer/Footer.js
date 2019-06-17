1beafdd08b6eba06d54536a161cabeb3875a72c8
SheriffHobo <zombiemaster@gmail.com>
2019-06-17 14:57:04 -0700

    login form css updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import {Component} from 'react';
 import "./Style.css";
 
 class Footer extends Component {
 
   render() {
-  return (
-    <div className="Footer-fixed">
-      <div className="">
-        <a><i id="menubtn" className="menu-btn noselect small material-icons dropdown-trigger" href='#' data-target='dropdown1'>menu</i></a>
-          <ul id='dropdown1' className='dropdown-content'>
-            <li><a href="#!">one</a></li>
-            <li><a href="#!">two</a></li>
-            <li className="divider" tabindex="-1"></li>
-            <li><a href="#!">three</a></li>
-            <li><a href="#!"><i className="material-icons">view_module</i>four</a></li>
-            <li><a href="#!"><i className="material-icons">cloud</i>five</a></li>
-          </ul>
-        <a><i id="homebtn" className="home-btn noselect small material-icons" onClick={() => alert('go to home')}>home</i></a>
-        <a><i id="chatbtn" className="chat-btn noselect small material-icons" onClick={() => alert('open chat')}>chat</i></a>
+    return (
+      <div className="Footer-fixed">
+          <i id="menubtn" className="menu-btn noselect small material-icons dropdown-trigger" href='#' data-target='dropdown1'>menu</i>
+            <ul id='dropdown1' className='dropdown-content'>
+              <li><Link to="/#">Activity Feed</Link></li>
+              <li><Link to="/#">Liked Shelves</Link></li>
+              <li><Link to="/#">Discover</Link></li>
+              <li className="divider" tabindex="-1"></li>
+              <li><Link to="/#">Account</Link></li>
+              <li className="divider" tabindex="-1"></li>
+              <li><Link to="/#">Terms</Link></li>
+              <li><Link to="/#">About</Link></li>
+            </ul>
+          <i id="homebtn" className="home-btn noselect small material-icons"><Link to="/#">home</Link></i>
+          <i id="chatbtn" className="chat-btn noselect small material-icons"><Link to="/#">chat</Link></i>
       </div>
-    </div>
     );
   };
 };
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import "./Style.css";
+import { Link } from "react-router-dom";
+import {Component} from 'react';
+import "./Style.css"
 
 const Login = React.memo(props => {
+  
+  //hey! Do you have live sshare audio? If so we can avoid having to run another app to communicate.
+  
+
+
 	return (
-		<h1>Login page</h1>
+
+  <main className="valign-wrapper">
+      <div classNameName="container">
+        <div classNameName="row">
+          <center>
+            <form method="post">
+              <div className='row'>
+                <div className='input-field'>
+                  <input className='validate' type='email' name='email' id='email' />
+                  <label for='email'>Enter your email</label>
+                </div>
+                <div className='input-field'>
+                  <input className='validate' type='password' name='password' id='password' />
+                  <label for='password'>Enter your password</label>
+                </div>
+                  <button type='submit' name='btn_login' className='btn_login btn-small'><Link to="/#">Login</Link></button>
+                  <button type='submit' name='btn_create' className='btn_create btn-small'><Link to="/#">Create Account</Link></button>
+                  <button type='submit' name='btn_forgot' className='btn_forgot btn-small'><Link to="/#">Forgot Password?</Link></button>
+              </div>
+            </form>
+          </center>
+        </div>
+      </div>
+  </main>
+  
 	);
 });
 
