159ccffee4a4455efeaab04c5dc0cfd33410d4bd
SheriffHobo <zombiemaster@gmail.com>
2019-06-17 22:00:32 -0700

    stash

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,46 +8,28 @@ const Header = React.memo(props => {
       
       <row id="headnav">
 
-
         <div className="Header-text">
           <Link to={'/'} className="Link">
             <h5>Alexandria</h5>
           </Link>
         </div>
 
-    {
-      props.loggedIn
-        ? <div className="add-btn noselect">
-                <i id="newshelfbtn" class="small material-icons">add_circle</i>
-          </div>
-        : <div /> 
-    }
-
-
         {
-
-          props.username
+          props.loggedIn
             ? <div className="add-btn noselect">
                     <i id="newshelfbtn" class="small material-icons">add_circle</i>
-
-          props.loggedIn
-            ? 'Hi, ' + props.username
-            : <div>
-                <Link to={'/login'} className="Link">Login | </Link>
-                <Link to={'/signup'} className="Link">Sign Up</Link>
-
               </div>
             : <div /> 
         }
 
-            {
-              props.username
-                ? <div className="Header-text">Hi, ' + props.username</div>
-                : <div className="Header-text">
-                    <Link to={'/login'} className="Link">Login | </Link>
-                    <Link to={'/signup'} className="Link">Sign Up</Link>
-                  </div>
-            }
+        {
+          props.username
+            ? <div className="Header-text">Hi, ' + props.username</div>
+            : <div className="Header-text">
+                <Link to={'/login'} className="Link">Login | </Link>
+                <Link to={'/signup'} className="Link">Sign Up</Link>
+              </div>
+        }
 
       </row>
 
