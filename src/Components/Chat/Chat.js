d33bc7a9c82363ecd85919a2e47a3032ed902879
hoangle4 <hoang.le4@pcc.edu>
2019-07-07 22:49:25 -0700

    commit redirect after create shelf

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,68 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import ioClient from 'socket.io-client';
-import { Button, Icon } from 'react-materialize'
-import { getCookie } from '../../API/cookies';
-import axios from 'axios';
-import './ChatStyle.css';
+import React, { useState, useEffect } from "react";
+import ioClient from "socket.io-client";
+import { Button, Icon } from "react-materialize";
+import { getCookie } from "../../API/cookies";
+import axios from "axios";
+import "./ChatStyle.css";
+import { conditionalExpression } from "@babel/types";
 
 const Chat = React.memo(props => {
-	const [message, setMessage] = useState('');
-	const [conversation, setConversation] = useState([]);
+  const [message, setMessage] = useState("");
+  const [conversation, setConversation] = useState([]);
 
-	useEffect(() => {
-		initIO();
+  useEffect(() => {
+    initIO();
+  }, []);
+  const socketUrl = "http://localhost:8080";
 
-	}, []);
-	const socketUrl = "/"
+  const initIO = () => {
+    const socket = ioClient.connect(socketUrl);
+    socket.on("messageSend", function(data) {
+      console.log(data);
+      setConversation(data);
+    });
+  };
 
+  const submitMessage = async event => {
+    const socket = ioClient.connect(socketUrl);
+    event.preventDefault();
+    const token = await getCookie("token");
 
-	const initIO = () => {
-		const socket = ioClient.connect(socketUrl);
-		socket.on("messageSend", function (data) {
-			setConversation(data);
-		});
-	}
+    await socket.emit("outputMsg", {
+      message,
+      token
+    });
+    setMessage("");
+  };
 
+  // const clearMessage = async () => {
+  // 	const result = await axios.delete('/api/chats/clear');
+  // 	console.log(result);
+  // }
 
-	const submitMessage = async (event) => {
-		const socket = ioClient.connect(socketUrl);
-		event.preventDefault();
-		const token = getCookie('token');
-
-		await socket.emit("outputMsg", {
-			message,
-			token,
-		});
-		setMessage('');
-	}
-
-	// const clearMessage = async () => {
-	// 	const result = await axios.delete('/api/chats/clear');
-	// 	console.log(result);
-	// }
-
-	return (
-		<div className="Chatmain">
-			<div className="Chat_Top">
-				{conversation.map(msg => (
-
-					<div id="chattextoutput">
-						<p><b>{msg.name}</b>:{' '}{msg.message}</p>
-					</div>
-
-				))}
-			</div>
-			{/* <br /> */}
-			<div className="Chat_Bottom">
-				<textarea
-					value={message}
-					id="chattextarea"
-					placeholder="Enter message."
-
-					onChange={e => setMessage(e.target.value)}
-				/>
-				<div className="Chat_Btn_Group">
-					{/* <button
+  return (
+    <div className="Chatmain">
+      <div className="Chat_Top">
+        {conversation.map(msg => (
+          <div id="chattextoutput">
+            <p>
+              <b>{msg.name}</b>: {msg.message}
+            </p>
+          </div>
+        ))}
+      </div>
+      {/* <br /> */}
+      <div className="Chat_Bottom">
+        <textarea
+          value={message}
+          id="chattextarea"
+          placeholder="Enter message."
+          onChange={e => setMessage(e.target.value)}
+        />
+        <div className="Chat_Btn_Group">
+          {/* <button
 						className="Chat_Clear_Btn"
 						onClick={() => {
 							setConversation([]);
@@ -71,13 +69,17 @@ const Chat = React.memo(props => {
 					>
 						Clear Chat History
                         </button> */}
-					<button className="Chat_Send_Btn small waves-effect waves-light" type="submit" onClick={submitMessage}>
-						Submit
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+          <button
+            className="Chat_Send_Btn small waves-effect waves-light"
+            type="submit"
+            onClick={submitMessage}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 });
 
 export default Chat;
--- src/Components/NewShelf/NewShelf.js
+++ src/Components/NewShelf/NewShelf.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import API from '../../API/API';
+import React, { useState, memo } from "react";
+import { Redirect } from "react-router-dom";
+import API from "../../API/API";
 import "./NewShelfStyle.css";
 
 // name: req.body.name,
@@ -13,86 +14,84 @@ import "./NewShelfStyle.css";
 // when i grow up i want to be a whole cabinet
 // hopes and dreams
 
-const NewShelf = React.memo(props => {
-  const [ name, setName ] = useState('');
-  const [ description, setDescription ] = useState('');
-  const [ customCategory, setCustomCategory ] = useState('');
+const NewShelf = memo(props => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [customCategory, setCustomCategory] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const submit = () => {
+  const submit = async () => {
     const shelf = { name, description, customCategory };
 
-    API.create(shelf, 'shelves')
-      .then(res => {
-        alert('congrats')
-        console.log('congrats',res)
-      })
-      .catch(err => console.error(err))
-  }
+    const result = await API.create(shelf, "shelves");
+    if (!result) return;
+    setIsSubmit(true);
+  };
+
+  if (isSubmit) return <Redirect to="/" />;
 
   return (
-		<main className="valign-wrapper newshelf">
-		  <center>
-			<form className="valign-wrapper newshelfform" method="post">
-			  <div className='row'>
+    <main className="valign-wrapper newshelf">
+      <center>
+        <form className="valign-wrapper newshelfform" method="post">
+          <div className="row">
+            <div className="input-field">
+              <input
+                type="text"
+                name="shelfname"
+                id="shelfname"
+                className="shelfinput"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+              <label htmlFor="shelfname" className="noselect">
+                Name your shelf
+              </label>
+            </div>
 
-  				<div className='input-field'>
-  				  <input
-              type='text'
-              name='shelfname'
-              id='shelfname'
-              className='shelfinput'
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
-  				  <label htmlFor='shelfname' className="noselect">
-              Name your shelf
-            </label>
-  				</div>
+            <div className="input-field">
+              <input
+                type="text"
+                name="shelfdesc"
+                id="shelfdesc"
+                className="shelfinput"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
+              <label htmlFor="shelfdesc" className="noselect">
+                Describe your shelf
+              </label>
+            </div>
 
-          <div className='input-field'>
-            <input
-              type='text'
-              name='shelfdesc'
-              id='shelfdesc'
-              className='shelfinput'
-              value={description}
-              onChange={e => setDescription(e.target.value)}
-            />
-            <label htmlFor='shelfdesc' className="noselect">
-              Describe your shelf
-            </label>
+            <div className="input-field">
+              <input
+                type="text"
+                name="shelfcustom"
+                id="shelfcustom"
+                className="shelfinput"
+                value={customCategory}
+                onChange={e => setCustomCategory(e.target.value)}
+              />
+              <label htmlFor="shelfcustom" className="noselect">
+                Choose a custom category
+              </label>
+            </div>
+
+            <button
+              name="btn_createshelf"
+              className="btn_createshelf btn-small waves-effect waves-light"
+              onClick={e => {
+                e.preventDefault();
+                submit();
+              }}
+            >
+              Create Shelf
+            </button>
           </div>
-
-          <div className='input-field'>
-            <input
-              type='text'
-              name='shelfcustom'
-              id='shelfcustom'
-              className='shelfinput'
-              value={customCategory}
-              onChange={e => setCustomCategory(e.target.value)}
-            />
-            <label htmlFor='shelfcustom' className="noselect">
-              Choose a custom category
-            </label>
-          </div>          
-
-  				<button
-            name='btn_createshelf'
-            className='btn_createshelf btn-small waves-effect waves-light'
-            onClick={e => {
-              e.preventDefault();
-              submit();
-            }}
-          >
-            Create Shelf
-          </button>
-
-			  </div>
-			</form>
-		  </center>
-		</main>
+        </form>
+      </center>
+    </main>
   );
 });
-  
+
 export default NewShelf;
