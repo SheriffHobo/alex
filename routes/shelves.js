f8326d25392ca34cad99d0e380557e36b21768af
Hoang Le <hoang.le4@pcc.edu>
2019-07-09 19:56:11 -0700

    commit login delay

--- routes/shelves.js
+++ routes/shelves.js
@@ -5,100 +5,98 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 router.get('/', auth, async (req, res) => {
-  let limit = 20;
-  const requestedLimit = Math.abs(parseInt(req.query.limit));
-  if (requestedLimit && requestedLimit <= limit) limit = requestedLimit;
+	let limit = 20;
+	const requestedLimit = Math.abs(parseInt(req.query.limit));
+	if (requestedLimit && requestedLimit <= limit) limit = requestedLimit;
+	const shelfId = req.query.shelfId;
+	const userId = req.query.userId;
+	const categoryId = req.query.categoryId;
 
-  const shelfId = req.query.shelfId;
-  const userId = req.query.userId;
-  const categoryId = req.query.categoryId;
+	const query = {
+		flagged: { $lt: 2 },
+		deleted: false,
+		private: false,
+		$or: []
+	};
 
-  const query = {
-    flagged: { $lt: 2 },
-    deleted: false,
-    private: false,
-    $or: [],
-  };
+	if (!req.user.allowNsfw) query.nsfw = false;
 
-  if (!req.user.allowNsfw) query.nsfw = false;
+	if (shelfId) {
+		if (!/^[a-fA-F0-9]{24}$/.test(shelfId)) {
+			return res.status(400).json({ message: `Invalid shelf ID.` });
+		}
 
-  if (shelfId) {
-    if (!/^[a-fA-F0-9]{24}$/.test(shelfId)) {
-      return res.status(400).json({ message: `Invalid shelf ID.`});
-    }
+		query.shelfId = mongoose.Types.ObjectId(shelfId);
+		return queryAndSend(res, query, limit);
+	}
 
-    query.shelfId = mongoose.Types.ObjectId(shelfId);
-    return queryAndSend(res, query, limit);
-  };
+	if (userId) {
+		if (!/^[a-fA-F0-9]{24}$/.test(userId)) {
+			return res.status(400).json({ message: `Invalid user ID.` });
+		}
 
-  if (userId) {
-    if (!/^[a-fA-F0-9]{24}$/.test(userId)) {
-      return res.status(400).json({ message: `Invalid user ID.`});
-    }
+		query.userId = mongoose.Types.ObjectId(userId);
+		return queryAndSend(res, query, limit);
+	}
 
-    query.userId = mongoose.Types.ObjectId(userId);
-    return queryAndSend(res, query, limit);
-  };
+	if (categoryId) {
+		if (!/^[a-fA-F0-9]{24}$/.test(categoryId)) {
+			return res.status(400).json({ message: `Invalid category ID.` });
+		}
 
-  if (categoryId) {
-    if (!/^[a-fA-F0-9]{24}$/.test(categoryId)) {
-      return res.status(400).json({ message: `Invalid category ID.`});
-    }
+		query.categoryId = mongoose.Types.ObjectId(categoryId);
+		return queryAndSend(res, query, limit);
+	}
 
-    query.categoryId = mongoose.Types.ObjectId(categoryId);
-    return queryAndSend(res, query, limit);
-  };
+	if (req.query.search) {
+		// VALIDATE
 
-  if (req.query.search) {
-    // VALIDATE
+		const search = new RegExp(req.query.search, 'i');
+		query.$or.push({ name: search }, { description: search });
 
-    const search = new RegExp(req.query.search, 'i');
-    query.$or.push({ name: search }, { description: search });
+		if (!req.query.category) return queryAndSend(res, query, limit);
+	}
 
-    if (!req.query.category) return queryAndSend(res, query, limit);
-  };
+	if (req.query.category) {
+		// VALIDATE
 
-  if (req.query.category) {
-    // VALIDATE
+		const category = new RegExp(req.query.category, 'i');
+		query.$or.push({ categoryName: category }, { customCategory: category });
 
-    const category = new RegExp(req.query.category, 'i');
-    query.$or.push({ categoryName: category }, { customCategory: category });
+		return queryAndSend(res, query, limit);
+	}
 
-    return queryAndSend(res, query, limit);
-  };
-
-  return res.status(400).json({ message: 'Invalid shelf request' });
+	return res.status(400).json({ message: 'Invalid shelf request' });
 });
 
 router.get('/me', auth, async (req, res) => {
-  let limit = 20;
-  const requestedLimit = Math.abs(parseInt(req.query.limit));
-  if (requestedLimit && requestedLimit <= limit) limit = requestedLimit;
+	let limit = 20;
+	const requestedLimit = Math.abs(parseInt(req.query.limit));
+	if (requestedLimit && requestedLimit <= limit) limit = requestedLimit;
 
-  const userId = mongoose.Types.ObjectId(req.user._id);
-  const query = { userId };
+	const userId = mongoose.Types.ObjectId(req.user._id);
+	const query = { userId };
 
-  return queryAndSend(res, query, limit);
+	return queryAndSend(res, query, limit);
 });
 
 async function queryAndSend(res, query, limit) {
-  if (query.$or && !query.$or.length) delete query.$or;
+	if (query.$or && !query.$or.length) delete query.$or;
 
-  const shelves = limit === 1
-    ? await Shelf.findOne(query)
-    : query._id
-    ? await Shelf.findById(query._id)
-    : await Shelf.find(query).limit(limit);
+	const shelves =
+		limit === 1
+			? await Shelf.findOne(query)
+			: query._id ? await Shelf.findById(query._id) : await Shelf.find(query).limit(limit);
 
-  res.status(200).json(shelves);
+	res.status(200).json(shelves);
 }
 
 router.post('/', auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).json(error.details[0].message);
+	const { error } = validate(req.body);
+	if (error) return res.status(400).json(error.details[0].message);
 
-  shelf = new Shelf({
-    name: req.body.name,
+	shelf = new Shelf({
+		name: req.body.name,
 		description: req.body.description,
 		categoryName: req.body.categoryName,
 		categoryId: req.body.categoryId,
@@ -106,12 +104,12 @@ router.post('/', auth, async (req, res) => {
 		// private: req.body.private,
 		// nsfw: req.body.nsfw,
 		image: req.body.image,
-		userId: mongoose.Types.ObjectId(req.user._id),
-  });
+		userId: mongoose.Types.ObjectId(req.user._id)
+	});
 
-  await shelf.save();
+	await shelf.save();
 
-  res.status(200).json(shelf);
+	res.status(200).json(shelf);
 });
 
 module.exports = router;
--- src/Components/UserButton/UserButton.js
+++ src/Components/UserButton/UserButton.js
@@ -1,62 +1,64 @@
-import React, { Fragment } from "react";
-import userAPI from "../../API/API";
-import Card from "../Card/Card";
+import React, { Fragment } from 'react';
+import userAPI from '../../API/API';
+import Card from '../Card/Card';
 
-const UserButton = React.memo(props => {
-  const user = props.data || {};
+const UserButton = React.memo((props) => {
+	const user = props.data || {};
 
-  let shelves = [];
-  if (props.selected)
-    shelves = props.shelves.map(shelf => {
-      return (
-        <Card
-          key={shelf.key}
-          selected={shelf._id === props.selectedShelf}
-          selectedChild={props.selectedChild}
-          data={shelf}
-          singular={"shelf"}
-          getChildren={props.getChildren}
-          items={props.items}
-          childCollection={"items"}
-        />
-      );
-    });
+	let shelves = [];
+	if (props.selected)
+		shelves = props.shelves.map((shelf) => {
+			return (
+				<Card
+					key={shelf.key}
+					selected={shelf._id === props.selectedShelf}
+					selectedChild={props.selectedChild}
+					data={shelf}
+					singular={'shelf'}
+					getChildren={props.getChildren}
+					items={props.items}
+					childCollection={'items'}
+				/>
+			);
+		});
 
-  const handleFollowing = async () => {
-    const result = await userAPI.followUser(user._id);
-  };
-  return (
-    <Fragment>
-      <div className="usersearchresultbtn noselect">
-        <div className="row userbtnrow">
-          <div className="col xl2">
-            {user.thumbnail ? (
-              <img
-                src={user.thumbnail}
-                id="stamp"
-                alt="user"
-                onClick={() => props.getChildren("shelves", "user", user._id)}
-              />
-            ) : (
-              <img
-                src="/pictures/stampicon.png"
-                id="stamp"
-                alt="user"
-                onClick={() => props.getChildren("shelves", "user", user._id)}
-              />
-            )}
-          </div>
-          <div className="col xl8">
-            <h6 className="shelftitle">{user.firstName}</h6>
-          </div>
-          <div className="col xl2">
-            <i className="fas fa-rss-square" onClick={handleFollowing} />
-          </div>
-        </div>
-      </div>
-      {shelves}
-    </Fragment>
-  );
+	const handleFollowing = async () => {
+		const result = await userAPI.followUser(user._id);
+		console.log(result.json());
+	};
+	return (
+		<Fragment>
+			<div className="usersearchresultbtn noselect">
+				<div className="row userbtnrow">
+					<div className="col xl2">
+						{user.thumbnail ? (
+							<img
+								src={user.thumbnail}
+								id="stamp"
+								alt="user"
+								onClick={() => props.getChildren('shelves', 'user', user._id)}
+							/>
+						) : (
+							<img
+								src="/pictures/stampicon.png"
+								id="stamp"
+								alt="user"
+								onClick={() => props.getChildren('shelves', 'user', user._id)}
+							/>
+						)}
+					</div>
+					<div className="col xl8">
+						<h6 className="shelftitle">{user.firstName}</h6>
+					</div>
+					<div className="col xl2">
+						<i className="fas fa-rss-square" onClick={handleFollowing} />
+						{/* <i class="fas fa-check-square" /> */}
+					</div>
+				</div>
+			</div>
+			{shelves}
+		</Fragment>
+	);
 });
 
 export default UserButton;
